4dc06a0be6cb4c89c2d9b9dccd84067700fc40d0
Lemrabt Boudadya <lboudadya@gmail.com>
2019-01-14 04:28:33 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -42,6 +42,15 @@ def selective_tweet_shortener(tweet)
   end
 end
 
+def shortened_tweet_truncator(tweet)
+  tweet.split(" ").collect do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)[0..140] + "..."
+    else
+      phrase
+    end
+  end.join(" ")
+end
 
 
 
