8ee4282b2fb57ede53117add04eccccfb60e4afe
Lemrabt Boudadya <lboudadya@gmail.com>
2019-01-14 04:11:33 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,7 +3,7 @@
   def word_substituter(tweet)
   # replaces long words with their expected short form
 
-    tweet.split(" ").map do |word|
+    tweet.split(" ").collect do |word|
       if dictionary.keys.include?(word.downcase)
         word = dictionary[word.downcase]
       else
